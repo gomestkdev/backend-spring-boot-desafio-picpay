b846151ff9ddd420cc8888c5924d803477e79a1c
Jose Gomes - gomestkdev <gomestkdev@gmail.com>
2026-06-22 19:51:49 -0300

    Add wallet repository

---
+++ src/main/java/com/github/gomestkd/backend_desafio_picpay/wallet/WalletRepository.java
@@ -0,0 +1,7 @@
+package com.github.gomestkd.backend_desafio_picpay.wallet;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface WalletRepository extends CrudRepository<Wallet, Long> {
+
+}
