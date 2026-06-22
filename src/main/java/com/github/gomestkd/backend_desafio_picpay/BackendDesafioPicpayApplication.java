f14cac44dae3b119743269b30e6698fbb60cd9f6
Jose Gomes - gomestkdev <gomestkdev@gmail.com>
2026-06-22 19:28:56 -0300

    Add initial project struct

---
+++ src/main/java/com/github/gomestkd/backend_desafio_picpay/BackendDesafioPicpayApplication.java
@@ -0,0 +1,13 @@
+package com.github.gomestkd.backend_desafio_picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDesafioPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDesafioPicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gomestkd/backend_desafio_picpay/BackendDesafioPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gomestkd.backend_desafio_picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDesafioPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
