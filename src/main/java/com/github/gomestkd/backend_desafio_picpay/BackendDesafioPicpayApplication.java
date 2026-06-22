571884fc3e5ee7300df01dfe35bb03fd0d5efd3f
Jose Gomes - gomestkdev <gomestkdev@gmail.com>
2026-06-22 19:40:43 -0300

    Add transaction entity

--- src/main/java/com/github/gomestkd/backend_desafio_picpay/BackendDesafioPicpayApplication.java
+++ src/main/java/com/github/gomestkd/backend_desafio_picpay/BackendDesafioPicpayApplication.java
@@ -2,7 +2,9 @@ package com.github.gomestkd.backend_desafio_picpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
 
+@EnableJdbcAuditing
 @SpringBootApplication
 public class BackendDesafioPicpayApplication {
 
---
+++ src/main/java/com/github/gomestkd/backend_desafio_picpay/transaction/Transaction.java
@@ -0,0 +1,22 @@
+package com.github.gomestkd.backend_desafio_picpay.transaction;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("TRANSACTIONS")
+public record Transaction(
+    @Id Long id,
+    Long payer,
+    Long payee,
+    BigDecimal value,
+    @CreatedDate LocalDateTime createdAt
+) {
+    public Transaction {
+        value = value.setScale(2, RoundingMode.HALF_EVEN);
+    }
+}
