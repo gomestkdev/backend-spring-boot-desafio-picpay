0c5f515a6ff1c920c9a9c614d373dcd2f02fc851
Jose Gomes - gomestkdev <gomestkdev@gmail.com>
2026-06-22 19:42:21 -0300

    Add wallet entity

---
+++ src/main/java/com/github/gomestkd/backend_desafio_picpay/wallet/Wallet.java
@@ -0,0 +1,17 @@
+package com.github.gomestkd.backend_desafio_picpay.wallet;
+
+import java.math.BigDecimal;
+
+import org.springframework.data.annotation.Id;
+
+public record Wallet(
+    @Id Long id,
+    String fullName,
+    Long cpf,
+    String email,
+    String password,
+    int type,
+    BigDecimal balance
+) {
+    
+}
